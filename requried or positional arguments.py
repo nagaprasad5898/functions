c9b730c8f76541f73f9d08c9b0010665a629852a
kannalucky <nagaprasad5898@gmail.com>
2021-09-03 18:26:26 +0530

    update

---
+++ requried or positional arguments.py
@@ -0,0 +1,7 @@
+def kanna(a,b):
+    c=a+b
+    print("printing inside the function 'c' value:",c)#if we dont use print to get the value of c use return
+    return c #if we dont use return function it will return none
+kanna(1,2) #if we use print we can directly call with function name
+print("calling the return value:",kanna(1,2))#if we want to print the return value
+# we want to use "print(function name(postional argiments))".
