3c9792f14d7ff90b0adf7b1892e4fa64f4dd75b2
kannalucky <nagaprasad5898@gmail.com>
2021-09-03 13:42:58 +0530

    update

--- basic structer.py
+++ basic structer.py
@@ -8,5 +8,5 @@ k=operations1(a=int(input("enter a number")),
 b=int(input("enter a number")),
 c=int(input("enter a number")),d=int(input("enter a number")))
 q,e=k
-print(q)
+print(e,q)
 #print(operations1(2,1,3,4))
