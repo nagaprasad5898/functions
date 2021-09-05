979f0a50158095dda1b6194874c2dda1b0eb9d2d
kannalucky <nagaprasad5898@gmail.com>
2021-09-05 20:07:34 +0530

    update

---
+++ examples.py
@@ -0,0 +1,12 @@
+#using of "/" and "*"and definding the parameters all the perameters before "/" are required
+# and after "/" are requried and keyword parameters and after "*" all are keyword parameters
+def typesofparameters(a,b,c,/,d,f=2,*,e=10,g=58):
+    print(a,b,c)
+    print(d,f)
+    print(e,g)
+typesofparameters(1,2,3,5)
+typesofparameters(1,2,4,d=5)
+typesofparameters(1,8,5,6,5)
+#typesofparameters(a=1,b=10,c=56,d=2,f=4,e=6,g=9)#typesofparameters()
+# got some positional-only arguments passed as keyword arguments: 'a, b, c'
+typesofparameters(1,8,5,d=6,f=5,e=5,g=6)
