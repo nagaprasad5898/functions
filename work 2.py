b9530be78b10c9ceb1954fb6d8264cb5e5dd0c2d
kannalucky <nagaprasad5898@gmail.com>
2021-09-06 12:38:50 +0530

    update

--- work 2.py
+++ work 2.py
@@ -1,20 +1,20 @@
 def vowel(a):
     if a in "aeiouAEIOU":
-        print("{} is vowel".format(a))
+        return "{} is vowel".format(a)
     else:
-        print("{} it is not a vowel".format(a))
-vowel("a")
+        return "{} it is not a vowel".format(a)
+print(vowel("a"))
 
 def sum(a):
     b=0
     for i in a:
         b=b+i
-    print(b)
-sum([1,2,3,4])
+    return b
+print(sum([1,2,3,4]))
 
 def mul(a):
     b=1
     for i in a:
         b=b*i
-    print(b)
-mul([1,2,3,4])
+    return b
+print(mul([1,2,3,4]))
